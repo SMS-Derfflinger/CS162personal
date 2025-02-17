ead92a047c38971863c6530ba33945eb8edd5ab5
Heinz <aurelianob808@gmail.com>
2025-02-17 06:46:29 +0000

    finish proj0

--- proj-pregame/src/userprog/process.c
+++ proj-pregame/src/userprog/process.c
@@ -100,8 +100,10 @@ static void start_process(void* file_name_) {
     if_.cs = SEL_UCSEG;
     if_.eflags = FLAG_IF | FLAG_MBS;
     success = load(file_name, &if_.eip, &if_.esp);
-  }
     if_.esp -= 20;
+    // set args's address's data default 1, if doesn't pass args argument will exit(1)
+    asm volatile("movl $1, 0x4(%0)" : : "g"(if_.esp) : "memory");
+  }
 
   /* Handle failure with succesful PCB malloc. Must free the PCB */
   if (!success && pcb_success) {
